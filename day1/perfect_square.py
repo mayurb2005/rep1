5f2dcff1e94d6e18e1212da3bd535feb0e201145
mayurb2005 <mayurareyou@gmail.com>
2025-02-25 04:01:11 +0000

    commited

--- day1/perfect_square.py
+++ day1/perfect_square.py
@@ -5,3 +5,12 @@ if root_number*root_number==input_number:
     print(input_number,'is a perfect square')
 else:
     print(input_number,'is not a perfect square')
+import math as mt
+
+input_number = int(input('Enter a number to check if it is Perfect Square: '))
+
+root_number = int(mt.sqrt(input_number))
+if root_number * root_number == input_number:
+    print(input_number, 'is a Perfect Square')
+else:
+    print(f'{input_number} is not a Perfect Square')
